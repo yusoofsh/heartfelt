ad3ad52c0a4e5b336f5d39db71923ef064b5044a
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-01 02:22:35 +0700

    🔧 config: optional NEXT_PUBLIC_APP_URL `.env`

--- src/env.mjs
+++ src/env.mjs
@@ -21,7 +21,7 @@ export const env = createEnv({
 	 * `NEXT_PUBLIC_`.
 	 */
 	client: {
-		NEXT_PUBLIC_APP_URL: z.string().min(1),
+		NEXT_PUBLIC_APP_URL: z.string().min(1).optional(),
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
 	},
 
