7c5f5c95660f313b3c442f9c5620943509124a16
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2023-05-01 19:40:47 +0700

    ♻️ refactor: move analytics component to providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,17 @@
 "use client"
 
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from "next-themes"
 
 import { api } from "~/lib/api/client"
 
 export function ClientProviders({ children }: PropsWithChildren) {
 	return (
-		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-			<api.Provider>{children}</api.Provider>
-		</ThemeProvider>
+		<>
+			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+				<api.Provider>{children}</api.Provider>
+			</ThemeProvider>
+			<Analytics />
+		</>
 	)
 }
